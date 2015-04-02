bdcf51cc67e5973554ed8b6196f52fa357c40fe5
PC Drew <pc@soprisapps.com>
2015-04-02 14:56:16 -0600

    Initial conversion to a target-based plugin instead of fixed locations for the points.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
-jQuery(document).ready(function($){
-	//open interest point description
-	$('.cd-single-point').children('a').on('click', function(){
-		var selectedPoint = $(this).parent('li');
-		if( selectedPoint.hasClass('is-open') ) {
-			selectedPoint.removeClass('is-open').addClass('visited');
-		} else {
-			selectedPoint.addClass('is-open').siblings('.cd-single-point.is-open').removeClass('is-open').addClass('visited');
-		}
-	});
+jQuery(document).ready(function ($) {
+    $('.cd-single-point').each(function () {
+        "use strict";
+        var $point = $(this),
+            $target = $($point.data("cd-target")),
+            pointLocation,
+            targetLocation,
+            targetWidth,
+            targetHeight,
+            newPosition = {};
+
+        if ($target.length > 0) {
+            targetLocation = $target.offset();
+            targetWidth = $target.width();
+            targetHeight = $target.height();
+
+            newPosition = targetLocation;
+
+            pointLocation = $point.data("cd-point-location").split(" ");
+            pointLocation.forEach(function (val) {
+                switch (val) {
+                    case "left":
+                        // do nothing, already set.
+                        break;
+                    case "center":
+                        newPosition.left += targetWidth / 2;
+                        break;
+                    case "right":
+                        newPosition.left += targetWidth;
+                        break;
+
+                    case "top":
+                        // do nothing, already set.
+                        break;
+                    case "middle":
+                        newPosition.top += targetHeight / 2;
+                        break;
+                    case "bottom":
+                        newPosition.top += targetHeight;
+                        break;
+                }
+            });
+
+            $point.offset(newPosition);
+
+            $point.children('a').on('click', function () {
+                var selectedPoint = $(this).parent('div');
+                if (selectedPoint.hasClass('is-open')) {
+                    selectedPoint.removeClass('is-open').addClass('visited');
+                } else {
+                    selectedPoint.addClass('is-open').siblings('.cd-single-point.is-open').removeClass('is-open').addClass('visited');
+                }
+            });
+        }
+    });
+
 	//close interest point description
 	$('.cd-close-info').on('click', function(event){
 		event.preventDefault();
